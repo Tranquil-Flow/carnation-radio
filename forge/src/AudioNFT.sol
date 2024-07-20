85abe9039e81c2a92fac36e631257d8ad034e4d6
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-07-20 02:09:32 +0200

    foundry init

---
+++ forge/src/AudioNFT.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AudioNFT is ERC721 {
+    // Mapping from token ID to token URI
+    mapping(uint256 => string) private _tokenURIs;
+    uint256 private _currentTokenId = 0;
+
+    // Address of the Auction contract
+    address public auctionContract;
+
+    error OnlyAuctionContract();
+
+    constructor(address _auctionContract) ERC721("CarnationRadio AudioNFT", "CARNATION_AUDIO") {
+        auctionContract = _auctionContract;
+    }
+
+    modifier onlyAuction() {
+        if (msg.sender != auctionContract) {
+            revert OnlyAuctionContract();
+        }
+        _;
+    }
+
+    function mint(address to, string memory _tokenURI) external onlyAuction returns (uint256) {
+        _currentTokenId++;
+        uint256 newTokenId = _currentTokenId;
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+        return newTokenId;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+}
