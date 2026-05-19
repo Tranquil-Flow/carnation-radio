32fe5ab6c7534e825de360f7ae7b01f8eedda7ba
Tranquil-Flow <tranquil_flow@protonmail.com>
2026-05-19 21:42:34 +0000

    Fix live audio decode path

--- frontend/app/components/AudioPlayer.tsx
+++ frontend/app/components/AudioPlayer.tsx
@@ -5,9 +5,11 @@ import { forwardRef } from 'react'
 interface Props {
   src: string | null
   onPlay?: () => void
+  onReady?: () => void
+  controlsEnabled?: boolean
 }
 
-const AudioPlayer = forwardRef<HTMLAudioElement, Props>(({ src, onPlay }, ref) => {
+const AudioPlayer = forwardRef<HTMLAudioElement, Props>(({ src, onPlay, onReady, controlsEnabled = true }, ref) => {
   if (!src) return null
 
   return (
@@ -15,8 +17,11 @@ const AudioPlayer = forwardRef<HTMLAudioElement, Props>(({ src, onPlay }, ref) =
       <audio
         ref={ref}
         src={src}
-        controls
+        preload="auto"
+        controls={controlsEnabled}
         onPlay={onPlay}
+        onLoadedMetadata={onReady}
+        onCanPlay={onReady}
         className="w-full"
         style={{ filter: 'invert(1)' }}
       />
